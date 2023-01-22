9e69715467d2f65659ac366d0f73d6c84849ac5d
Derry <thanhhaivevo@gmail.com>
2023-01-22 15:44:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loadData: [
+
+      ],
+
+
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://14.225.254.113:8000/datauser')
+      .then(res => {
+        const loadData = res.data;
+        this.setState({ loadData });
+        console.log(loadData)
+      })
+
+
+  }
+
+
+
+  render() {
+
+    return (
+      <div style={{ backgroundColor: 'gray' }}>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Họ và tên</th>
+              <th scope="col">Tiền trong người</th>
+              <th scope="col">Tiền trong thẻ</th>
+              <th scope="col">Cấp độ</th>
+
+
+            </tr>
+          </thead>
+          {this.state.loadData.map((item, key) => {
+            return (
+              <tbody >
+                <tr style={{ fontSize: '15px', color: 'white', marginTop: '2vh', borderBottom: 'none', marginTop: '1vh' }}>
+                  <th scope="row">{item.id}</th>
+                  <td>{item.name}</td>
+                  <td>{item.money}</td>
+                  <td>{item.money_bank}</td>
+
+                  <td> Level {item.work_lvl}</td>
+                </tr>
+
+              </tbody>
+
+            )
+          })}
+
+        </table>
+
+      </div>
+
+    )
+  }
 }
 
 export default App;
